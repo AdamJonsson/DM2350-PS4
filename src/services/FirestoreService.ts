27d205462deb2801c580f8eaa6b735cf9e363d2b
= <adamsmajl@gmail.com>
2020-09-21 22:12:50 +0200

    Fix config for production

--- src/services/FirestoreService.ts
+++ src/services/FirestoreService.ts
@@ -5,12 +5,21 @@ export class FirestoreService {
     static database: firebase.firestore.Firestore;
     
     static establishDBConnectionToFirestoreProject() {
-        const firebaseConfig = {
-            apiKey: process.env.REACT_APP_DOOR_SURVEY_FIREBASE_API_KEY,
-            authDomain: process.env.REACT_APP_DOOR_SURVEY_FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.REACT_APP_DOOR_SURVEY_FIREBASE_PROJECT_ID
-        };
+        const firebaseConfig = this.getFirebaseConfig();
         firebase.initializeApp(firebaseConfig);
         this.database = firebase.firestore();
+    }
+
+    private static getFirebaseConfig() {
+        if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+            return {
+                apiKey: process.env.REACT_APP_DOOR_SURVEY_FIREBASE_API_KEY,
+                authDomain: process.env.REACT_APP_DOOR_SURVEY_FIREBASE_AUTH_DOMAIN,
+                projectId: process.env.REACT_APP_DOOR_SURVEY_FIREBASE_PROJECT_ID
+            }
+        } else {
+            if (process.env.FIREBASE_CONFIG)
+                return JSON.parse(process.env.FIREBASE_CONFIG);
+        }
     }
 }
