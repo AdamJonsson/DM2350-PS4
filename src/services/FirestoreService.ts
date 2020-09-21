0b5b1588c8904878b4a59356550c012508d94069
= <adamsmajl@gmail.com>
2020-09-21 22:25:33 +0200

    Add config in public repository

--- src/services/FirestoreService.ts
+++ src/services/FirestoreService.ts
@@ -1,25 +1,22 @@
 import firebase from "firebase";
+import 'firebase-functions';
 
 export class FirestoreService {
 
     static database: firebase.firestore.Firestore;
     
     static establishDBConnectionToFirestoreProject() {
-        const firebaseConfig = this.getFirebaseConfig();
-        firebase.initializeApp(firebaseConfig);
+        firebase.initializeApp({
+            apiKey: "AIzaSyCg2ZG7UwX3q7EdBepCl2clYu_2OUkPysY",
+            authDomain: "doorsurvey-10eb6.firebaseapp.com",
+            databaseURL: "https://doorsurvey-10eb6.firebaseio.com",
+            projectId: "doorsurvey-10eb6",
+            storageBucket: "doorsurvey-10eb6.appspot.com",
+            messagingSenderId: "828097104176",
+            appId: "1:828097104176:web:f6959512ed626ddf97a0f5",
+            measurementId: "G-SC6X8Y6V7J"
+        });
         this.database = firebase.firestore();
     }
 
-    private static getFirebaseConfig() {
-        if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-            return {
-                apiKey: process.env.REACT_APP_DOOR_SURVEY_FIREBASE_API_KEY,
-                authDomain: process.env.REACT_APP_DOOR_SURVEY_FIREBASE_AUTH_DOMAIN,
-                projectId: process.env.REACT_APP_DOOR_SURVEY_FIREBASE_PROJECT_ID
-            }
-        } else {
-            if (process.env.FIREBASE_CONFIG)
-                return JSON.parse(process.env.FIREBASE_CONFIG);
-        }
-    }
 }
